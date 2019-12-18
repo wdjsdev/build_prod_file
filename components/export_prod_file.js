ff9eaec4257a65dc81b65f5812a6f813f90422bc
wdjsdev <wdjsdev@gmail.com>
2019-12-18 11:15:09 -0600

    updated artboard creation for more accurate artboard bounds

--- components/export_prod_file.js
+++ components/export_prod_file.js
@@ -93,8 +93,30 @@ function exportProdFile(pdfFolderName, destFolder)
 		}
 
 		piece.selected = true;
-		doc.fitArtboardToSelectedArt(0);
+		
+		// doc.fitArtboardToSelectedArt(0);
+
+		function makeArtboard(group,rmItems)
+		{
+			var doc = app.activeDocument
+			var dupGroup = group.duplicate();
+			for(var x = dupGroup.pageItems.length - 1; x>=0; x--)
+			{
+				if(rmItems.indexOf(dupGroup.pageItems[x].name)>-1)
+				{
+					dupGroup.pageItems[x].remove();
+				}
+			}
+			
+			doc.selection = null;
+			dupGroup.selected = true;
+			doc.fitArtboardToSelectedArt(0);
+			dupGroup.remove();
+		}
+		makeArtboard(piece,["Roster","Live Text"]);
+
 		app.executeMenuCommand("fitall");
+
 
 		colorBlocks();
 

1318ede0c69c7ebad77b121db2f049801e368960
williams <31516287+wdjsdev@users.noreply.github.com>
2022-03-22 10:54:18 -0500

    resurrect legacy build prod file

---
+++ Build_Prod_File_Dev.jsx
@@ -0,0 +1,163 @@
+/*
+
+Script Name: Build_Prod_File
+Author: William Dowling
+Build Date: 23 February, 2018
+Description: query netsuite for JSON data for a given order number,
+				parse the data, create new document, copy necessary
+				artwork to new document, input roster data
+	
+	
+*/
+
+#target Illustrator
+function container()
+{
+	var valid = true;
+	var scriptName = "build_prod_file";
+
+	function isDrUser()
+	{
+		var files = Folder("/Volumes/").getFiles();
+
+		for(var x=0;x<files.length;x++)
+		{
+			if(files[x].name.toLowerCase().indexOf("customizationdr")>-1)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	function getUtilities()
+	{
+		var result = [];
+		var utilPath = "/Volumes/" + (isDrUser() ? "CustomizationDR" : "Customization") + "/Library/Scripts/Script_Resources/Data/";
+		var ext = ".jsxbin"
+
+		//check for dev utilities preference file
+		var devUtilitiesPreferenceFile = File("~/Documents/script_preferences/dev_utilities.txt");
+
+		if(devUtilitiesPreferenceFile.exists)
+		{
+			devUtilitiesPreferenceFile.open("r");
+			var prefContents = devUtilitiesPreferenceFile.read();
+			devUtilitiesPreferenceFile.close();
+			if(prefContents.match(/true/i))
+			{
+				utilPath = "~/Desktop/automation/utilities/";
+				ext = ".js";
+			}
+		}
+
+		if($.os.match("Windows"))
+		{
+			utilPath = utilPath.replace("/Volumes/","//AD4/");
+		}
+
+		result.push(utilPath + "Utilities_Container" + ext);
+		result.push(utilPath + "Batch_Framework" + ext);
+
+		if(!result.length)
+		{
+			valid = false;
+			alert("Failed to find the utilities.");
+		}
+		return result;
+
+	}
+
+	var utilities = getUtilities();
+	for(var u=0,len=utilities.length;u<len;u++)
+	{
+		eval("#include \"" + utilities[u] + "\"");	
+	}
+
+	if(!valid)return;
+
+	logDest.push(getLogDest());
+
+
+	/*****************************************************************************/
+	//=================================  Data  =================================//
+	
+	
+
+
+	//=================================  /Data  =================================//
+	/*****************************************************************************/
+
+
+
+	/*****************************************************************************/
+	//==============================  Components  ===============================//
+
+	var devComponents = desktopPath + "/automation/build_prod_file/components";
+	var prodComponents = componentsPath + "/build_prod_file";
+
+	var compFiles = includeComponents(devComponents,prodComponents,false);
+	if(compFiles.length)
+	{
+		var curComponent;
+		for(var cf=0,len=compFiles.length;cf<len;cf++)
+		{
+			curComponent = compFiles[cf].fullName;
+			eval("#include \"" + curComponent + "\"");
+			log.l("included: " + compFiles[cf].name);
+		}
+	}
+	else
+	{
+		errorList.push("Failed to find the necessary components.");
+		log.e("No components were found.");
+		valid = false;
+		return valid;
+	}
+
+	
+
+	//=============================  /Components  ===============================//
+	/*****************************************************************************/
+
+
+
+	/*****************************************************************************/
+	//=================================  Procedure  =================================//
+	
+	
+
+	if(valid)
+	{
+		initBuildProd();
+	}
+
+	if(valid)
+	{
+		valid = masterLoop();
+	}
+	
+	// buildStats.buildScriptExecutionTime = timer.calculate();
+
+
+	//=================================  /Procedure  =================================//
+	/*****************************************************************************/
+
+	if(errorList.length)
+	{
+		sendErrors(errorList);
+	}
+
+	if(messageList.length)
+	{
+		sendScriptMessages(messageList);
+	}
+
+	printLog();
+
+	return valid;
+
+}
+
+container();
+

a9f4e3b543371e258405fa22b9e7cf8511b47a32
williams <31516287+wdjsdev@users.noreply.github.com>
2022-08-26 15:37:40 -0500

    Update get_base_color.js
    formatting and better handling of compoundPath edge case

--- components/get_base_color.js
+++ components/get_base_color.js
@@ -1,25 +1,28 @@
 
 
-function getBaseColor(curGarmentLayer) {
+function getBaseColor ( curGarmentLayer )
+{
     var doc = app.activeDocument;
     doc.selection = null;
     var baseColor = "";
-    
-    var mockLay = findSpecificLayer(curGarmentLayer,"Mockup","any");
-    if(!mockLay)return;
-    var paramLay = findSpecificLayer(mockLay,"paramcolors","any");
-    if(!paramLay)return;
-    var c1Block = findSpecificPageItem(paramLay,"c1","any");
-    if(!c1Block)return;
+
+    var mockLay = findSpecificLayer( curGarmentLayer, "Mockup", "any" );
+    if ( !mockLay ) return;
+    var paramLay = findSpecificLayer( mockLay, "paramcolors", "any" );
+    if ( !paramLay ) return;
+    var c1Block = findSpecificPageItem( paramLay, "paramcolor-c1", "imatch" );
+    if ( !c1Block ) return;
 
     var sandboxLayer = doc.layers.add();
-    var testBlock = c1Block.duplicate(sandboxLayer);
+    var testBlock = c1Block.duplicate( sandboxLayer );
     testBlock.selected = true;
-    app.executeMenuCommand("expandStyle");
+    app.executeMenuCommand( "expandStyle" );
     doc.selection = null;
-    
-    var bg = sandboxLayer.pageItems[sandboxLayer.pageItems.length-1];
-    if(bg.filled && bg.fillColor && bg.fillColor.spot && bg.fillColor.spot.name.match(/ b[\d]?$/i))
+
+    var bg = sandboxLayer.pageItems[ sandboxLayer.pageItems.length - 1 ];
+
+    bg = bg.typename.match( /compound/i ) ? bg.pathItems[ 0 ] : bg;
+    if ( bg.filled && bg.fillColor && bg.fillColor.spot && bg.fillColor.spot.name.match( / b[\d]?$/i ) )
     {
         baseColor = bg.fillColor.spot.name;
     }
